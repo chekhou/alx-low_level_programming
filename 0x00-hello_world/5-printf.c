3f68338799e2e8e0054b1b0fd1d042f5230a2c5a
cfaye385@yahoo.com <cfaye385@yahoo.com>
2022-09-08 13:09:38 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  * Return: 0 to terminate program
  */
 int main(void)
-{
-        printf("with proper grammar, but the outcome is a piece of art,\n"); 
-        return (0);
+{  
+printf("with proper grammar, but the outcome is a piece of art,\n"); 
+return (0);
 }
