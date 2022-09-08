37c325547e993549ad8f66ce006aefeca28673dd
cfaye385@yahoo.com <cfaye385@yahoo.com>
2022-09-08 09:57:26 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 /**
- *
  * main - Print string in console
  *
- *   
- *
  * Return: 0 to terminate program
- *
  */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
