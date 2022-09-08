5aaa4532e90e24e28b60bcbae7fc0935663932f6
cfaye385@yahoo.com <cfaye385@yahoo.com>
2022-09-08 13:29:43 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Print string in console
  * 
@@ -7,6 +6,6 @@
  */
 int main(void)
 {  
-        printf("with proper grammar, but the outcome is a piece of art,\n"); 
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n"); 
+	return (0);
 }
