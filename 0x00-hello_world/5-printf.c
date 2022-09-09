f847acd6ae3d9ccf7358ad81ea0483f04eb574eb
cfaye385@yahoo.com <cfaye385@yahoo.com>
2022-09-09 04:45:35 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Print string in console
+ * main - Entry point
  * 
- * Return: 0 to terminate program
+ *Return: Always 0
  */
-int main(void)
+int main (void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n"); 
+	printf(" with proper grammar, but the outcome is a piece of art, \n");
+
 	return (0);
 }
